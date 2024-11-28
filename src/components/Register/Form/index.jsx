f4dd6d045f387eb801dd1c1e788d2a25746d7652
Dhiyaul Faruq <117615379+pentahead@users.noreply.github.com>
2024-11-28 22:56:47 +0700

    Dev (#24)
    * templates
    
    * feat: interface Authentification (Login, Register, Forget Password, OTP) (#13)
    
    * Feature authentification (#15)
    
    * feat: interface Authentification (Login, Register, Forget Password, OTP)
    
    * feat: service auth
    
    * Feature payment (#16)
    
    * feat: interface Authentification (Login, Register, Forget Password, OTP)
    
    * feat: interface payment
    
    * Feature account (#17)
    
    * feature: Pengaturan akun & ubah profil
    
    * add hide password
    
    * Feature pemesanan (#20)
    
    * Dev (#14)
    
    * templates
    
    * feat: interface Authentification (Login, Register, Forget Password, OTP) (#13)
    
    * Feature: create pemesanan
    
    * fixing cinflict
    
    ---------
    
    Co-authored-by: Dhiyaul Faruq <117615379+pentahead@users.noreply.github.com>
    
    * Feature authentification (#23)
    
    * feat: interface Authentification (Login, Register, Forget Password, OTP)
    
    * feat: service auth
    
    * feat: integration Authentication
    
    * resolve build error
    
    ---------
    
    Co-authored-by: M. Fadhillah Rahman <167836894+FadPro77@users.noreply.github.com>
    Co-authored-by: Naila Jinan Gaisani <146344930+nellnelaa@users.noreply.github.com>

--- src/components/Register/Form/index.jsx
+++ src/components/Register/Form/index.jsx
@@ -1,3 +1,18 @@
+<<<<<<< HEAD
+import React, { useRef, useState } from "react";
+import "../../../styles/variables.scss";
+import { Link, useNavigate } from "@tanstack/react-router";
+import { useDispatch, useSelector } from "react-redux";
+import { Container, Row, Col, Button, Form, Image } from "react-bootstrap";
+import { IoEyeOutline, IoEyeOffOutline } from "react-icons/io5";
+import { useMutation } from "@tanstack/react-query";
+import { register } from "../../../service/auth";
+<<<<<<< HEAD
+import { setToken } from "../../../redux/slices/auth";
+import toast, { Toaster } from "react-hot-toast";
+=======
+>>>>>>> e680011bd640e47652230a69286d127236c70cc5
+=======
 import React from "react";
 import "../../../styles/variables.scss";
 import { Link, useNavigate } from "@tanstack/react-router";
@@ -6,6 +21,7 @@ import { useState } from "react";
 import { Container, Row, Col, Button, Form, Image } from "react-bootstrap";
 import { IoEyeOutline, IoEyeOffOutline } from "react-icons/io5";
 import { register } from "../../../service/auth";
+>>>>>>> 7f7fa05f70d763199338adb18dbf44ecfe9e641a
 
 const RegisterForm = () => {
   const dispatch = useDispatch();
@@ -20,7 +36,150 @@ const RegisterForm = () => {
 
   const [type, setType] = useState("password");
   const [icon, setIcon] = useState(<IoEyeOffOutline />);
+<<<<<<< HEAD
+
+  const [errors, setErrors] = useState({
+    firstName: "",
+    lastName: "",
+    phone: "",
+    email: "",
+    password: "",
+  });
+  const firstNameRef = useRef(null);
+  const lastNameRef = useRef(null);
+  const phoneRef = useRef(null);
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const confirmPasswordRef = useRef(null);
+
+  const { mutate: registerUser } = useMutation({
+    mutationFn: (request) => {
+      return register(request);
+    },
+    onSuccess: (result) => {
+      if (result?.meta) {
+        dispatch(setToken(result?.data?.token));
+        toast.success(result?.meta?.message, {
+          style: {
+            padding: "16px",
+            background: "#73CA5C",
+            color: "#FFFFFF",
+          },
+          iconTheme: {
+            primary: "#000",
+            secondary: "#fff",
+          },
+        });
+        navigate({ to: "/otp", state: { email: email.trim() } });
+      } else {
+        handleApiError(result.message);
+      }
+    },
+    onError: (err) => {
+      handleApiError(err.message);
+      toast.error(err?.message, {
+        style: {
+          padding: "16px",
+          background: "#FF0000",
+          color: "#FFFFFF",
+        },
+        iconTheme: {
+          primary: "#000",
+          secondary: "#fff",
+        },
+      });
+    },
+  });
+
+  const handleApiError = (errorMessage) => {
+    const message = errorMessage?.toLowerCase() || "";
+    console.log(message);
+
+    setErrors({ email: "", password: "" });
+    if (message.includes("email")) {
+      setErrors((prev) => ({ ...prev, email: errorMessage }));
+      emailRef.current?.focus();
+    }
+    if (message.includes("password") || message.includes("credential")) {
+      setErrors((prev) => ({ ...prev, password: errorMessage }));
+      passwordRef.current?.focus();
+    }
+    if (message.includes("phone")) {
+      setErrors((prev) => ({ ...prev, phone: errorMessage }));
+      phoneRef.current?.focus();
+    }
+
+    // if (message.includes("password") || message.includes("credential")) {
+    //   setErrors((prev) => ({ ...prev, password: errorMessage }));
+    //   lastNameRef.current?.focus();
+    // }
+
+    // if (message.includes("password") || message.includes("")) {
+    //   setErrors((prev) => ({ ...prev, password: errorMessage }));
+    //   passwordRef.current?.focus();
+    // } else {
+    //   setErrors((prev) => ({ ...prev, email: errorMessage }));
+    // }
+  };
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    setErrors({
+      firstName: "",
+      lastName: "",
+      phone: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    });
+
+    if (!firstName) {
+      setErrors((prev) => ({ ...prev, firstName: "Nama depan harus diisi" }));
+      firstNameRef.current?.focus();
+      return;
+    }
+    if (!lastName) {
+      setErrors((prev) => ({ ...prev, lastName: "Nama belakang harus diisi" }));
+      lastNameRef.current?.focus();
+      return;
+    }
+    if (!email) {
+      setErrors((prev) => ({ ...prev, email: "Email harus diisi" }));
+      emailRef.current?.focus();
+      return;
+    }
+    if (!phone) {
+      setErrors((prev) => ({ ...prev, phone: "Nomor Hp harus diisi" }));
+      phoneRef.current?.focus();
+      return;
+    }
+
+    if (!password) {
+      setErrors((prev) => ({ ...prev, password: "Password harus diisi" }));
+      passwordRef.current?.focus();
+      return;
+    }
+    if (password !== confirmPassword) {
+      setErrors((prev) => ({
+        ...prev,
+        confirmPassword: "Password tidak sama!",
+      }));
+      confirmPasswordRef.current?.focus();
+      return;
+    }
+
+    const request = {
+      firstName,
+      lastName,
+      phone,
+      email: email.trim(),
+      password,
+    };
+    registerUser(request);
+  };
+=======
   const [validated, setValidated] = useState(false);
+>>>>>>> 7f7fa05f70d763199338adb18dbf44ecfe9e641a
 
   const handleEyeToggle = () => {
     if (type === "password") {
@@ -32,6 +191,11 @@ const RegisterForm = () => {
     }
   };
 
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+=======
+>>>>>>> 7f7fa05f70d763199338adb18dbf44ecfe9e641a
   const handleRegister = async (e) => {
     e.preventDefault();
 
@@ -63,6 +227,10 @@ const RegisterForm = () => {
       navigate({ to: "/otp" });
     }
   };
+<<<<<<< HEAD
+>>>>>>> e680011bd640e47652230a69286d127236c70cc5
+=======
+>>>>>>> 7f7fa05f70d763199338adb18dbf44ecfe9e641a
   return (
     <div
       style={{
@@ -70,6 +238,31 @@ const RegisterForm = () => {
         width: "100%",
       }}
     >
+<<<<<<< HEAD
+<<<<<<< HEAD
+      {" "}
+=======
+>>>>>>> e680011bd640e47652230a69286d127236c70cc5
+      <h2 className="fw-bold text-start mb-4">Daftar</h2>
+      <Form noValidate onSubmit={handleRegister}>
+        {/* Nama */}
+        <Form.Group className="mb-3" controlId="validationCustom01">
+          <Form.Label>Nama Depan</Form.Label>
+          <Form.Control
+            ref={firstNameRef}
+            type="text"
+            placeholder="Nama Depan"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            isInvalid={!!errors.firstName}
+            style={{
+              borderRadius: "15px",
+              padding: "1em",
+            }}
+          />
+          <Form.Control.Feedback type="invalid">
+            {errors.firstName}
+=======
       <h2 className="fw-bold text-start mb-4">Daftar</h2>
       <Form noValidate validated={validated} onSubmit={handleRegister}>
         {/* Nama */}
@@ -90,10 +283,27 @@ const RegisterForm = () => {
           </div>
           <Form.Control.Feedback type="invalid">
             Please provide a valid city.
+>>>>>>> 7f7fa05f70d763199338adb18dbf44ecfe9e641a
           </Form.Control.Feedback>
         </Form.Group>
         <Form.Group className="mb-3" controlId="validationCustom02">
           <Form.Label>Nama Belakang</Form.Label>
+<<<<<<< HEAD
+          <Form.Control
+            ref={lastNameRef}
+            isInvalid={!!errors.lastName}
+            type="text"
+            placeholder="Nama Belakang"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            style={{
+              borderRadius: "15px",
+              padding: "1em",
+            }}
+          />
+          <Form.Control.Feedback type="invalid">
+            {errors.lastName}
+=======
           <div style={{ position: "relative" }}>
             <Form.Control
               type="text"
@@ -109,12 +319,30 @@ const RegisterForm = () => {
           </div>
           <Form.Control.Feedback type="invalid">
             Please provide a valid city.
+>>>>>>> 7f7fa05f70d763199338adb18dbf44ecfe9e641a
           </Form.Control.Feedback>
         </Form.Group>
 
         {/* Email */}
         <Form.Group className="mb-3" controlId="validationCustom03">
           <Form.Label>Email</Form.Label>
+<<<<<<< HEAD
+          <Form.Control
+            type="email"
+            ref={emailRef}
+            isInvalid={!!errors.email}
+            placeholder="Contoh: johndoe@gmail.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            style={{
+              borderRadius: "15px",
+              padding: "1em",
+              paddingRight: "2.5em",
+            }}
+          />
+          <Form.Control.Feedback type="invalid">
+            {errors.email}
+=======
           <div style={{ position: "relative" }}>
             <Form.Control
               type="email"
@@ -131,12 +359,29 @@ const RegisterForm = () => {
           </div>
           <Form.Control.Feedback type="invalid">
             Please provide a valid email.
+>>>>>>> 7f7fa05f70d763199338adb18dbf44ecfe9e641a
           </Form.Control.Feedback>
         </Form.Group>
 
         {/* Nomor Telepon */}
         <Form.Group className="mb-3" controlId="validationCustom04">
           <Form.Label>Nomor Telepon</Form.Label>
+<<<<<<< HEAD
+          <Form.Control
+            ref={phoneRef}
+            isInvalid={!!errors.phone}
+            type="number"
+            placeholder="+62 "
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+            style={{
+              borderRadius: "15px",
+              padding: "1em",
+            }}
+          />
+          <Form.Control.Feedback type="invalid">
+            {errors.phone}
+=======
           <div style={{ position: "relative" }}>
             <Form.Control
               type="number"
@@ -152,6 +397,7 @@ const RegisterForm = () => {
           </div>
           <Form.Control.Feedback type="invalid">
             Please provide a valid phone number.
+>>>>>>> 7f7fa05f70d763199338adb18dbf44ecfe9e641a
           </Form.Control.Feedback>
         </Form.Group>
 
@@ -161,6 +407,10 @@ const RegisterForm = () => {
           <div style={{ position: "relative" }}>
             <Form.Control
               type={type}
+<<<<<<< HEAD
+              ref={passwordRef}
+=======
+>>>>>>> 7f7fa05f70d763199338adb18dbf44ecfe9e641a
               placeholder="Masukkan password"
               required
               style={{
@@ -170,24 +420,43 @@ const RegisterForm = () => {
               }}
               value={password}
               onChange={(e) => setPassword(e.target.value)}
+<<<<<<< HEAD
+              isInvalid={!!errors.password}
+=======
               autoComplete="off"
+>>>>>>> 7f7fa05f70d763199338adb18dbf44ecfe9e641a
             />
             <span
               onClick={handleEyeToggle}
               style={{
                 position: "absolute",
+<<<<<<< HEAD
+                top: errors.password ? "35%" : "50%",
+                right: errors.password ? "40px" : "10px",
+                transform: "translateY(-50%)",
+                cursor: "pointer",
+                zIndex: 2,
+=======
                 top: "50%",
                 right: validated ? "40px" : "10px",
                 transform: "translateY(-50%)",
                 cursor: "pointer",
+>>>>>>> 7f7fa05f70d763199338adb18dbf44ecfe9e641a
               }}
             >
               {icon}
             </span>
+<<<<<<< HEAD
+            <Form.Control.Feedback type="invalid">
+              {errors.password}
+            </Form.Control.Feedback>
+          </div>
+=======
           </div>
           <Form.Control.Feedback type="invalid">
             Please choose a password.
           </Form.Control.Feedback>
+>>>>>>> 7f7fa05f70d763199338adb18dbf44ecfe9e641a
         </Form.Group>
 
         {/* Konfirmasi Password */}
@@ -195,9 +464,15 @@ const RegisterForm = () => {
           <Form.Label>Konfirmasi Password</Form.Label>
           <div style={{ position: "relative" }}>
             <Form.Control
+<<<<<<< HEAD
+              ref={confirmPasswordRef}
+              type={type}
+              placeholder="Konfirmasi password"
+=======
               type={type}
               placeholder="Konfirmasi password"
               required
+>>>>>>> 7f7fa05f70d763199338adb18dbf44ecfe9e641a
               style={{
                 borderRadius: "15px",
                 paddingRight: "40px",
@@ -205,24 +480,43 @@ const RegisterForm = () => {
               }}
               value={confirmPassword}
               onChange={(e) => setConfirmPassword(e.target.value)}
+<<<<<<< HEAD
+              isInvalid={!!errors.confirmPassword}
+=======
               autoComplete="off"
+>>>>>>> 7f7fa05f70d763199338adb18dbf44ecfe9e641a
             />
             <span
               onClick={handleEyeToggle}
               style={{
                 position: "absolute",
+<<<<<<< HEAD
+                top: errors.confirmPassword ? "35%" : "50%",
+                right: errors.confirmPassword ? "40px" : "10px",
+                transform: "translateY(-50%)",
+                cursor: "pointer",
+                zIndex: 2,
+=======
                 top: "50%",
                 right: validated ? "40px" : "10px",
                 transform: "translateY(-50%)",
                 cursor: "pointer",
+>>>>>>> 7f7fa05f70d763199338adb18dbf44ecfe9e641a
               }}
             >
               {icon}
             </span>
+<<<<<<< HEAD
+            <Form.Control.Feedback type="invalid">
+              {errors.confirmPassword}
+            </Form.Control.Feedback>
+          </div>
+=======
           </div>
           <Form.Control.Feedback type="invalid">
             Please confirm your password.
           </Form.Control.Feedback>
+>>>>>>> 7f7fa05f70d763199338adb18dbf44ecfe9e641a
         </Form.Group>
 
         <Button
@@ -262,6 +556,22 @@ const RegisterForm = () => {
           </Button>
         </p>
       </div>
+<<<<<<< HEAD
+      <div>
+        <Toaster
+          position="bottom-center"
+          containerStyle={{
+            position: "fixed",
+            bottom: "20px",
+            left: "75%",
+            transform: "translateX(-50%)",
+            zIndex: "9999",
+          }}
+          reverseOrder={false}
+        />
+      </div>
+=======
+>>>>>>> 7f7fa05f70d763199338adb18dbf44ecfe9e641a
     </div>
   );
 };
--- src/components/Register/OTP/index.jsx
+++ src/components/Register/OTP/index.jsx
@@ -1,3 +1,100 @@
+<<<<<<< HEAD
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col, Button, Form } from "react-bootstrap";
+import { useNavigate, useLocation } from "@tanstack/react-router";
+import OtpInput from "react-otp-input";
+import { IoArrowBack } from "react-icons/io5";
+import { verifyEmail } from "../../../service/auth";
+import { useMutation } from "@tanstack/react-query";
+import toast, { Toaster } from "react-hot-toast";
+
+const OTPForm = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const email = location.state?.email || "";
+
+  const [otp, setOtp] = useState("");
+  const [timer, setTimer] = useState(60);
+
+  useEffect(() => {
+    let countdown;
+    if (timer > 0) {
+      countdown = setInterval(() => {
+        setTimer((prev) => prev - 1);
+      }, 1000);
+    }
+    return () => clearInterval(countdown);
+  }, [timer]);
+
+  const { mutate: verifyRegister } = useMutation({
+    mutationFn: (request) => {
+      return verifyEmail(request);
+    },
+    onSuccess: (result) => {
+      if (result?.meta) {
+        // dispatch(setToken(result?.data?.token));
+        toast.success(result?.meta?.message, {
+          style: {
+            padding: "16px",
+            background: "#73CA5C",
+            color: "#FFFFFF",
+          },
+          iconTheme: {
+            primary: "#000",
+            secondary: "#fff",
+          },
+        });
+        navigate({ to: "/login" });
+      } else {
+        toast.error(result?.error?.message, {
+          style: {
+            padding: "16px",
+            background: "#FF0000",
+            color: "#FFFFFF",
+          },
+          iconTheme: {
+            primary: "#000",
+            secondary: "#fff",
+          },
+        });
+      }
+    },
+    onError: (err) => {
+      handleApiError(err.message);
+      toast.error(err?.message, {
+        style: {
+          padding: "16px",
+          background: "#FF0000",
+          color: "#FFFFFF",
+        },
+        iconTheme: {
+          primary: "#000",
+          secondary: "#fff",
+        },
+      });
+    },
+  });
+  const maskEmail = (email) => {
+    if (!email) return "";
+    const [username, domain] = email.split("@");
+    const maskedUsername = username.charAt(0) + "*".repeat(username.length - 1);
+    return `${maskedUsername}@${domain}`;
+  };
+
+  const handleResendOtp = () => {
+    setTimer(60);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const request = {
+      email: email,
+      otp,
+    };
+
+    verifyRegister(request);
+  };
+=======
 import React, { useState } from "react";
 import { Container, Row, Col, Button, Form } from "react-bootstrap";
 import { useNavigate } from "@tanstack/react-router";
@@ -8,11 +105,19 @@ const OTPForm = () => {
   const navigate = useNavigate();
 
   const [otp, setOtp] = useState("");
+>>>>>>> 7f7fa05f70d763199338adb18dbf44ecfe9e641a
 
   const handleBack = () => {
     navigate({ to: "/register" });
   };
 
+<<<<<<< HEAD
+  return (
+    <Container className="d-flex flex-column align-items-center mt-5">
+      <div>
+        <Toaster position="bottom-center" reverseOrder={false} />
+      </div>
+=======
   const handleSubmit = (e) => {
     e.preventDefault();
     alert(`OTP Anda: ${otp.join("")}`);
@@ -20,6 +125,7 @@ const OTPForm = () => {
 
   return (
     <Container className="d-flex flex-column align-items-center mt-5">
+>>>>>>> 7f7fa05f70d763199338adb18dbf44ecfe9e641a
       <div className="d-flex justify-content-start w-100 mb-4">
         <IoArrowBack
           onClick={handleBack}
@@ -31,11 +137,19 @@ const OTPForm = () => {
         />
         <h2 className="fw-bold text-end">Masukkan OTP</h2>
       </div>
+<<<<<<< HEAD
+      <p className="text-muted m-4">
+        Ketik 6 digit kode yang dikirimkan ke <b>{maskEmail(email)}</b>
+      </p>
+      <Form onSubmit={handleSubmit} className="w-100 justify-content-center">
+        <Row className="justify-content-center m-4">
+=======
       <p className="text-muted mt-4">
         Ketik 6 digit kode yang dikirimkan ke <b>j*****@gmail.com</b>
       </p>
       <Form onSubmit={handleSubmit} className="w-100">
         <Row className="justify-content-center mb-3">
+>>>>>>> 7f7fa05f70d763199338adb18dbf44ecfe9e641a
           <OtpInput
             value={otp}
             onChange={setOtp}
@@ -48,6 +162,47 @@ const OTPForm = () => {
               fontWeight: "bold",
               marginLeft: "1em",
               borderRadius: "14px",
+<<<<<<< HEAD
+              borderColor: "#D0D0D0",
+            }}
+            renderInput={(props) => <input {...props} />}
+          />
+        </Row>
+        <Row className="justify-content-center text-center mt-4">
+          {timer > 0 ? (
+            <p className="text-muted mt-4">
+              Kirim Ulang OTP dalam {timer} detik
+            </p>
+          ) : (
+            <p
+              className="mt-4"
+              style={{ color: "red", fontWeight: "bold", cursor: "pointer" }}
+              onClick={handleResendOtp}
+            >
+              Kirim Ulang
+            </p>
+          )}
+        </Row>
+        <Row className="justify-content-center mt-4">
+          <Button
+            variant="primary"
+            type="submit"
+            className="w-50 mb-4 mt-4"
+            style={{
+              backgroundColor: "#7126B5",
+              border: "none",
+              transition: "opacity 0.3s ease",
+              padding: "15px",
+              borderRadius: "14px",
+            }}
+            onMouseEnter={(e) => (e.currentTarget.style.opacity = "0.5")}
+            onMouseLeave={(e) => (e.currentTarget.style.opacity = "1")}
+          >
+            Simpan
+          </Button>
+        </Row>
+      </Form>
+=======
             }}
             renderInput={(props) => <input {...props} />}
           />{" "}
@@ -68,6 +223,7 @@ const OTPForm = () => {
         </Button>
       </Form>
       <p className="text-muted mt-2">Kirim Ulang OTP dalam 60 detik</p>
+>>>>>>> 7f7fa05f70d763199338adb18dbf44ecfe9e641a
     </Container>
   );
 };
