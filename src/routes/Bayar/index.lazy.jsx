ee52eb501c0d0f78016cae536cd717e0eac65f60
Naila Jinan Gaisani <146344930+nellnelaa@users.noreply.github.com>
2024-11-29 22:44:12 +0700

    Feature booking (#32)
    * Feature: create pemesanan
    
    * fixing cinflict
    
    * test
    
    * Feature: update seat picker

--- src/routes/Bayar/index.lazy.jsx
+++ src/routes/Bayar/index.lazy.jsx
@@ -13,3 +13,19 @@ function RouteComponent() {
     </NavbarPemesanan>
   )
 }
+
+import * as React from 'react'
+import { createLazyFileRoute } from '@tanstack/react-router'
+import NavbarPemesanan from "../../components/Navbar-pemesanan";
+
+export const Route = createLazyFileRoute('/Bayar/')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return (
+    <NavbarPemesanan >
+
+    </NavbarPemesanan>
+  )
+}
