f123bbaeae7e0d9820cb5708395c7b56bd91ca75
nellnelaa <nainaijinan@gmail.com>
2024-11-29 22:52:51 +0700

    feature: booking

--- src/routes/Bayar/index.lazy.jsx
+++ src/routes/Bayar/index.lazy.jsx
@@ -14,18 +14,3 @@ function RouteComponent() {
   )
 }
 
-import * as React from 'react'
-import { createLazyFileRoute } from '@tanstack/react-router'
-import NavbarPemesanan from "../../components/Navbar-pemesanan";
-
-export const Route = createLazyFileRoute('/Bayar/')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return (
-    <NavbarPemesanan >
-
-    </NavbarPemesanan>
-  )
-}
