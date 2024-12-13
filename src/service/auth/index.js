d2964aeeec52ba043d6afafc38bb85266c327d18
pentahead <dhiyaafaruq@gmail.com>
2024-12-13 20:04:30 +0700

    fix: Google Oauth service

--- src/service/auth/index.js
+++ src/service/auth/index.js
@@ -26,7 +26,7 @@ export const login = async (request) => {
 
 export const googleLogin = async (accessToken) => {
   const response = await fetch(
-    `${import.meta.env.VITE_API_URL}/auth/login-google`,
+    `${import.meta.env.VITE_API_URL}/api/v1/auth/login-google`,
     {
       body: JSON.stringify({ access_token: accessToken }),
       method: "POST",
