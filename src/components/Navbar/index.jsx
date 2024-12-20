645bb26bf7a6afc8cbfcbb78003b7bdd7365dfcc
pentahead <dhiyaafaruq@gmail.com>
2024-12-21 01:55:54 +0700

    refactor: add footer in layout booking and payment

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,13 +1,8 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import Offcanvas from "react-bootstrap/Offcanvas";
-import Image from "react-bootstrap/Image";
-import Dropdown from "react-bootstrap/Dropdown";
-import { Link, useNavigate } from "@tanstack/react-router";
-import { useEffect, useCallback } from "react";
+import { Container, Offcanvas, Navbar, Nav } from "react-bootstrap";
+import { Link } from "@tanstack/react-router";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setToken, setUser } from "../../redux/slices/auth";
+import { setUser } from "../../redux/slices/auth";
 import { profile } from "../../service/auth";
 import { useQuery } from "@tanstack/react-query";
 import { IoIosNotificationsOutline, IoIosList } from "react-icons/io";
@@ -16,7 +11,6 @@ import { RxPerson } from "react-icons/rx";
 
 const NavigationBar = () => {
   const dispatch = useDispatch();
-  const navigate = useNavigate();
   const { user, token } = useSelector((state) => state.auth);
 
   const { data, isSuccess, isError } = useQuery({
