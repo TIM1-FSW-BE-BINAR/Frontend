d2bf8778a8db791cc2821b120dfc4df0647cf56c
Naila Jinan Gaisani <146344930+nellnelaa@users.noreply.github.com>
2024-12-24 01:57:27 +0700

    Feature booking (#111)
    * Feature: create pemesanan
    
    * fixing cinflict
    
    * test
    
    * Feature: update seat picker
    
    * feature: booking
    
    * refactor: route booking & payment
    
    * refactor : implement API
    
    * refactor: integrasi BE
    
    * resolve conflict
    
    * resolve conflict payment
    
    * resolve: form orderData
    
    * refactor: API Integration
    
    * refactor: return flight
    
    * refactor: booking layout
    
    * refactor: service auth
    
    * refactor: timer booking
    
    * refactor: fix discount
    
    * fix discount
    
    * fix discount
    
    * refactor : form & ticket details
    
    * refactor: button

--- src/components/Booking/BookingForm.jsx
+++ src/components/Booking/BookingForm.jsx
@@ -323,8 +323,6 @@ function BookingForm({ setIsSaved, isSaved, setIsPayment, isPayment }) {
       bookingDetail,
     };
 
-    console.log("Request in JSON format:", JSON.stringify(request, null, 2));
-
     setDataBooking(request);
     toast.success("Data successfully saved.", {
       autoClose: 3000,
@@ -830,7 +828,7 @@ function BookingForm({ setIsSaved, isSaved, setIsPayment, isPayment }) {
                   <Button
                     type="submit"
                     variant="purple"
-                    className=" px-4 py-2 w-100"
+                    className="btn btn-block px-4 py-2 w-100 animated-button"
                     style={{
                       backgroundColor: isSaved ? "#D0D0D0" : "#6a1b9a",
                       borderColor: isSaved ? "#D0D0D0" : "#6a1b9a",
